c82740c603c49b00fdba6fd250086a219a905ce4
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-05-31 10:50:13 -0300

    Feito o cadastro e a logica de upload de imagem

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjetoGamer_MVC.Infra;
+using ProjetoGamer_MVC.Models;
 
 namespace ProjetoGamer_MVC.Controllers
 {
@@ -23,6 +24,59 @@ namespace ProjetoGamer_MVC.Controllers
             ViewBag.Equipe = c.Equipe.ToList();
             //retorna a view de equipe(TELA)
             return View();
+        }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            //instancia do objeto equipe
+            Equipe novaEquipe = new Equipe();
+
+            //atribuição de valores recebidos do formulário
+            novaEquipe.Nome = form["Nome"].ToString();
+
+            //aqui estava chegando como string
+            // novaEquipe.Imagem = form["Imagem"].ToString();
+
+
+            //início da lógica de upload da imagem
+            if (form.Files.Count > 0)
+            {
+
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                //gera o caminho completo até o caminho do arquivo(imagem - nome da extensão)
+                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwoot/img/",folder,file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+            //fim da lógica de upload
+
+            //adiciona objeto na tabela do BD
+            c.Equipe.Add(novaEquipe);
+ 
+            //salva as alterações feitas do BD
+            c.SaveChanges();
+
+            //Retorna para o local chamando a rota de listar(método index)
+            return LocalRedirect("~/Equipe/Listar");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
